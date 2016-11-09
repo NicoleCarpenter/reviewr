86f254d733d692e0f5ac6687f30cf1b2cfdb4501
Hana Lee <hanalee07@gmail.com>
2016-11-09 08:18:18 -0600

    Fix Javascript spec

--- app/assets/javascripts/rating.js
+++ app/assets/javascripts/rating.js
@@ -14,11 +14,11 @@ submitNewRatingForm = function(review_id, callback) {
 showRandomRatingForm = function(review_id, user_id, thumb) {
   var param = addThumbParam(thumb);
   if (param == '') {
-    param = "?random=true";
+    param = "?random=true&user=" + user_id;
   } else {
-    param = param + "&random=true";
+    param = param + "&random=true&user=" + user_id;
   }
-  showRatingForm(review_id, user_id, param)
+  showRatingForm(review_id, param)
 };
 
 addThumbParam = function(thumb) {
@@ -32,12 +32,12 @@ addThumbParam = function(thumb) {
   return param;
 };
 
-showRatingForm = function(review_id, user_id, param) {
+showRatingForm = function(review_id, param) {
   var callback = function(response) {
     dom.displayPartial("#new-rating", response);
     dom.hideElement("#new-rating-box");
     dom.hideElement("#rating-criteria");
   };
-  client.ajaxGet("/ratings/new/" + review_id + param + "&user=" + user_id,
+  client.ajaxGet("/ratings/new/" + review_id + param,
                  callback);
 }
