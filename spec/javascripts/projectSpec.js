9fb0bf22caa02db68f186f5d3c15b147891ea28d
Nicole <nicolecarpenterusa@gmail.com>
2016-11-17 09:25:23 -0600

    Enable invitee deletion in new project form

--- spec/javascripts/projectSpec.js
+++ spec/javascripts/projectSpec.js
@@ -5,12 +5,45 @@ describe("Projects", function() {
     server = sinon.fakeServer.create();
   });
 
-  describe("addInviteField()", function() {
+  describe("addField()", function() {
     it("loads a new email field to invite a reviewer", function() {
       affix("#invites");
-      addInviteField(); 
+      affix("#new-invite-field div#content");
+      addField("#invites", "#new-invite-field"); 
        
-      expect($("#invites").html()).toEqual('<input type="email" name="invites[email]">');
+      expect($("#invites").html()).toEqual('<div id="content"></div>');
+    });
+  });
+
+  describe("assignIds()", function() {
+    it("creates a numbered element id", function() {
+      affix(".element");
+      affix(".element");
+      assignIds(".element", "my_prefix_");
+       
+      expect($('.element#my_prefix_0').length).toEqual(1);
+      expect($('.element#my_prefix_1').length).toEqual(1);
+      expect($('.element#my_prefix_2').length).toEqual(0);
+
+    });
+  });
+
+
+  describe("removeElement()", function() {
+    it("removes an element from the DOM", function() {
+      affix(".element#my_prefix_1");
+      removeElement("#my_prefix_", 1);
+      
+      expect($('.element#my_prefix_1').length).toEqual(0); 
+    });
+  });
+
+  describe("getIdIndex()", function() {
+    it("gets the index from an element's id", function() {
+      affix(".element#my_prefix_200");
+      var index = getIdIndex(".element", "my_prefix_");
+
+      expect(index).toEqual('200');
     });
   });
 });
